bb7c75ebac42e76c7731e5bdbdf5e26ff5654c5c
Miquel Cubel <mcubel@gmail.com>
2011-10-24 17:52:58 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrawnTest::Application.config.secret_token = 'b6a8fc4e3276cf4aec1ea5e2bac1281832e944a7889a686b98d81094fac57c9d5d8b6697d17512729dda5771ed49ef9618ad79f83200ba265769bd61f316fd89'
